0394709abed69c4f5a64a9e9531b01041f4af379
Santiago Ariel <santiagoarielv98@gmail.com>
2023-12-26 14:24:15 -0300

    Add new HTML file and update dependencies

--- pages.ts
+++ pages.ts
@@ -1,16 +1,19 @@
-const pages: string[] = ["blog-preview-card"];
+import { resolve } from "path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const pages = ["blog-preview-card"];
 
 /**
  * Retrieves the pages and their corresponding file paths.
  * @returns An object containing the pages and their file paths.
  */
-export default function getPages(): Record<string, string> {
+export default function getPages() {
   const input = {
-    main: "/index.html",
+    main: resolve(__dirname, "index.html"),
   };
-
   pages.forEach((page) => {
-    input[page] = `/${page}/index.html`;
+    input[page] = resolve(__dirname, `src/${page}/index.html`);
   });
   return input;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import getPages from "./pages";
 
 export default defineConfig({
-  base: "",
-  root: "src",
   build: {
     rollupOptions: {
       input: getPages(),

b1d96dff0973022992e73fdb165944d7bc031100
Santiago Ariel <santiagoarielv98@gmail.com>
2023-12-26 11:32:57 -0300

    Add .prettierrc and update file paths

--- pages.ts
+++ pages.ts
@@ -6,10 +6,10 @@ const pages: string[] = ["blog-preview-card"];
  */
 export default function getPages(): Record<string, string> {
   const input = {
-    main: "index.html",
+    main: "/index.html",
   };
   pages.forEach((page) => {
-    input[page] = `${page}.html`;
+    input[page] = `/${page}/index.html`;
   });
   return input;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import getPages from "./pages";
 
 export default defineConfig({
+  root: "./src",
   build: {
     rollupOptions: {
       input: getPages(),
     },
+    outDir: "../dist",
   },
 });

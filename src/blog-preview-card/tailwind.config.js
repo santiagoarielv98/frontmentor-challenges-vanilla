793e72e04e8276c0fcda0ab7114afdda14b4fb88
Santiago Ariel <santiagoarielv98@gmail.com>
2023-12-26 19:24:32 -0300

    Update styles and add template HTML file

--- src/blog-preview-card/tailwind.config.js
+++ src/blog-preview-card/tailwind.config.js
@@ -1,4 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // src\blog-preview-card\index.html
   content: ["./src/blog-preview-card/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-yellow": "#FFD400",
+        "primary-grey": "#808080",
+      },
+    },
+  },
 };
